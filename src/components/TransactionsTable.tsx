44181c637573fe188a7c97624d5b325363cc378a
Marinavdac <marinavdac@gmail.com>
2022-11-20 10:14:58 -0300

    feat: add stripe

---
+++ src/components/TransactionsTable.tsx
@@ -0,0 +1,23 @@
+const TransactionsTable = () => {
+  return (
+    <table>
+    <thead>
+        <tr>
+            <th colSpan={2}>The table header</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>The table body</td>
+            <td>with two columns</td>
+            <td>with two columns</td>
+            <td>with two columns</td>
+            <td>with two columns</td>
+            <td>with two columns</td>
+        </tr>
+    </tbody>
+</table>
+  )
+}
+
+export default TransactionsTable;
